e75c0f4beb435690e8596c875c95a89f2bacd793
KamenSentai <cvt.alain@gmail.com>
2020-01-21 14:27:49 +0100

    feat(images): add media

--- src/utils/resources.js
+++ src/utils/resources.js
@@ -6,6 +6,7 @@ import AcneStudiosMockup from '@/assets/images/AcneStudios/Mockup.jpg'
 import AcneStudiosScreen1 from '@/assets/images/AcneStudios/Screen_1.jpg'
 import AcneStudiosScreen2 from '@/assets/images/AcneStudios/Screen_2.jpg'
 import AcneStudiosScreen3 from '@/assets/images/AcneStudios/Screen_3.jpg'
+import AcneStudiosScreen4 from '@/assets/images/AcneStudios/Screen_4.jpg'
 import AcneStudiosTransition from '@/assets/images/AcneStudios/Transition.jpg'
 
 import MealthyCover from '@/assets/images/Mealthy/Cover.jpg'
@@ -18,6 +19,10 @@ import MealthyScreen2 from '@/assets/images/Mealthy/Screen_2.png'
 import MealthyScreen3 from '@/assets/images/Mealthy/Screen_3.png'
 import MealthyScreen4 from '@/assets/images/Mealthy/Screen_4.png'
 import MealthyScreen5 from '@/assets/images/Mealthy/Screen_5.png'
+import MealthyScreen6 from '@/assets/images/Mealthy/Screen_6.png'
+import MealthyScreen7 from '@/assets/images/Mealthy/Screen_7.png'
+import MealthyScreen8 from '@/assets/images/Mealthy/Screen_8.png'
+import MealthyScreen9 from '@/assets/images/Mealthy/Screen_9.png'
 import MealthyTransition from '@/assets/images/Mealthy/Transition.jpg'
 
 import PocketNurseCover from '@/assets/images/PocketNurse/Cover.jpg'
@@ -25,9 +30,10 @@ import PocketNurseHero1 from '@/assets/images/PocketNurse/Hero_1.jpg'
 import PocketNurseHero2 from '@/assets/images/PocketNurse/Hero_2.jpg'
 import PocketNurseHero3 from '@/assets/images/PocketNurse/Hero_3.jpg'
 import PocketNurseMockup from '@/assets/images/PocketNurse/Mockup.jpg'
-import PocketNurseScreen1 from '@/assets/images/PocketNurse/Screen_1.jpg'
-import PocketNurseScreen2 from '@/assets/images/PocketNurse/Screen_2.jpg'
+import PocketNurseScreen1 from '@/assets/images/PocketNurse/Screen_1.mp4'
+import PocketNurseScreen2 from '@/assets/images/PocketNurse/Screen_2.mp4'
 import PocketNurseScreen3 from '@/assets/images/PocketNurse/Screen_3.jpg'
+import PocketNurseScreen4 from '@/assets/images/PocketNurse/Screen_4.jpg'
 import PocketNurseTransition from '@/assets/images/PocketNurse/Transition.jpg'
 
 import YukeyCover from '@/assets/images/Yukey/Cover.jpg'
@@ -35,10 +41,16 @@ import YukeyHero1 from '@/assets/images/Yukey/Hero_1.jpg'
 import YukeyHero2 from '@/assets/images/Yukey/Hero_2.jpg'
 import YukeyHero3 from '@/assets/images/Yukey/Hero_3.jpg'
 import YukeyMockup from '@/assets/images/Yukey/Mockup.jpg'
-import YukeyScreen1 from '@/assets/images/Yukey/Screen_1.jpg'
-import YukeyScreen2 from '@/assets/images/Yukey/Screen_2.jpg'
-import YukeyScreen3 from '@/assets/images/Yukey/Screen_3.jpg'
-import YukeyScreen4 from '@/assets/images/Yukey/Screen_4.jpg'
+import YukeyScreen1 from '@/assets/images/Yukey/Screen_1.mp4'
+import YukeyScreen2 from '@/assets/images/Yukey/Screen_2.mp4'
+import YukeyScreen3 from '@/assets/images/Yukey/Screen_3.mp4'
+import YukeyScreen4 from '@/assets/images/Yukey/Screen_4.mp4'
+import YukeyScreen5 from '@/assets/images/Yukey/Screen_5.jpg'
+import YukeyScreen6 from '@/assets/images/Yukey/Screen_6.jpg'
+import YukeyScreen7 from '@/assets/images/Yukey/Screen_7.jpg'
+import YukeyScreen8 from '@/assets/images/Yukey/Screen_8.jpg'
+import YukeyScreen9 from '@/assets/images/Yukey/Screen_9.jpg'
+import YukeyScreen10 from '@/assets/images/Yukey/Screen_10.jpg'
 import YukeyTransition from '@/assets/images/Yukey/Transition.jpg'
 
 export default {
@@ -50,6 +62,7 @@ export default {
   AcneStudiosScreen1,
   AcneStudiosScreen2,
   AcneStudiosScreen3,
+  AcneStudiosScreen4,
   AcneStudiosTransition,
 
   MealthyCover,
@@ -62,6 +75,10 @@ export default {
   MealthyScreen3,
   MealthyScreen4,
   MealthyScreen5,
+  MealthyScreen6,
+  MealthyScreen7,
+  MealthyScreen8,
+  MealthyScreen9,
   MealthyTransition,
 
   PocketNurseCover,
@@ -72,6 +89,7 @@ export default {
   PocketNurseScreen1,
   PocketNurseScreen2,
   PocketNurseScreen3,
+  PocketNurseScreen4,
   PocketNurseTransition,
 
   YukeyCover,
@@ -83,5 +101,11 @@ export default {
   YukeyScreen2,
   YukeyScreen3,
   YukeyScreen4,
+  YukeyScreen5,
+  YukeyScreen6,
+  YukeyScreen7,
+  YukeyScreen8,
+  YukeyScreen9,
+  YukeyScreen10,
   YukeyTransition,
 }
