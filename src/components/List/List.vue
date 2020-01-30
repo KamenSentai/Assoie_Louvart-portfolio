051d6d0a592f5241983b28af5c858c3574d535b6
KamenSentai <cvt.alain@gmail.com>
2020-01-31 00:45:24 +0100

    feat(list): build component

---
+++ src/components/List/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div :class="$style.container">
+    <ComponentTag :text="title" />
+    <ComponentParagraph
+      v-for="(item, index) in items"
+      :key="`item-${index}`"
+      :text="item"
+    />
+  </div>
+</template>
+
+<script>
+import { Tag as ComponentTag } from '@/components/Tag'
+import { Paragraph as ComponentParagraph } from '@/components/Paragraph'
+
+export default {
+  name: 'List',
+  components: {
+    ComponentTag,
+    ComponentParagraph,
+  },
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-gap: 1rem;
+  align-content: flex-start;
+}
+</style>
