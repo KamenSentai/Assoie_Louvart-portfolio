7fa9cc669f8ae11d4e5a3ad00e05a3a242683572
KamenSentai <cvt.alain@gmail.com>
2020-01-31 00:46:27 +0100

    feat(screen): build component

---
+++ src/components/Screen/Screen.vue
@@ -0,0 +1,25 @@
+<template>
+  <img
+    v-if="src"
+    :class="$style.image"
+    :src="src"
+  >
+</template>
+
+<script>
+export default {
+  name: 'Screen',
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.image {
+  width: 100%;
+}
+</style>
