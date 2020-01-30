05cb9f3401bcc90667d17a9a65e9185868bcab57
KamenSentai <cvt.alain@gmail.com>
2020-01-31 00:44:06 +0100

    feat(banner): build component

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    v-if="content.title || content.text"
+    :class="$style.container"
+  >
+    <ComponentTitle :text="content.title" />
+    <ComponentParagraph :text="content.text" />
+  </div>
+</template>
+
+<script>
+import { Paragraph as ComponentParagraph } from '@/components/Paragraph'
+import { Title as ComponentTitle } from '@/components/Title'
+
+export default {
+  name: 'Tag',
+  components: {
+    ComponentParagraph,
+    ComponentTitle,
+  },
+  props: {
+    content: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  display: grid;
+  grid-gap: 12rem;
+  grid-template-columns: 1fr 50%;
+  justify-content: space-between;
+  margin: 0 12rem 14rem;
+
+  @include bp(md) {
+    grid-gap: 4rem;
+    margin: 0 4rem 8rem;
+  }
+
+  @include bp(sm) {
+    grid-auto-flow: row;
+    margin: 0 3.5rem 4rem;
+  }
+}
+</style>
