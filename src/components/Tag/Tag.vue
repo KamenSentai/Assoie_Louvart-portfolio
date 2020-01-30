bff941e14f0b588620312de6af25940aec09951a
KamenSentai <cvt.alain@gmail.com>
2020-01-31 00:46:39 +0100

    feat(tag): build component

---
+++ src/components/Tag/Tag.vue
@@ -0,0 +1,34 @@
+<template>
+  <span
+    v-if="text"
+    :class="$style.container"
+  >
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'Tag',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  display: inline-block;
+  margin-bottom: 1rem;;
+  font-weight: 600;
+  font-size: 1.8rem;
+  text-transform: uppercase;
+
+  @include bp(sm) {
+    font-size: 1.6rem;
+  }
+}
+</style>
