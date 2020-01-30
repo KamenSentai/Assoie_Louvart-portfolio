c3c7c6b585c196b3e88cbee22e38761b5783c7a9
KamenSentai <cvt.alain@gmail.com>
2020-01-31 00:48:16 +0100

    feat(projet): add all content

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,51 +1,71 @@
 <template>
-  <div>
-    <div :class="$style.intro">
-      <div :class="$style.tag">
-        Intro
+  <div
+    v-if="!isNotFound"
+    class="font-null"
+  >
+    <section :class="$style.intro">
+      <ComponentTag text="Intro" />
+      <ComponentTitle
+        :class="$style.title"
+        :text="project.intro.title"
+      />
+      <div :class="$style.wrapper">
+        <ComponentList
+          v-for="(list, index) in lists"
+          :key="`list-${index}`"
+          :title="list.title"
+          :items="list.items"
+        />
+        <ComponentParagraph :text="project.intro.text" />
       </div>
-      <h2 :class="$style.title">
-        {{ project.intro.title }}
-      </h2>
-      <div :class="$style.sheet">
-        <div :class="$style.section">
-          <div :class="$style.tag">
-            Role
-          </div>
-          <p
-            v-for="(role, index) in project.intro.roles"
-            :key="`role-${index}`"
-            :class="$style.text"
-          >
-            {{ role }}
-          </p>
-        </div>
-        <div :class="$style.section">
-          <div :class="$style.tag">
-            What
-          </div>
-          <p :class="$style.text">
-            {{ project.type }}
-          </p>
-          <p :class="$style.text">
-            {{ project.year }}
-          </p>
-        </div>
-        <div :class="$style.section">
-          <p :class="$style.text">
-            {{ project.intro.text }}
-          </p>
-        </div>
-      </div>
-    </div>
+    </section>
+    <template v-for="(section, index) in project.sections">
+      <ComponentScreen
+        :key="`screen-${index}`"
+        :src="section.screen"
+      />
+      <section
+        :key="`section-${index}`"
+        :class="$style.section"
+      >
+        <ComponentBanner :content="section.contents" />
+        <ComponentGallery
+          :size="project.size"
+          :media="section.media"
+        />
+      </section>
+    </template>
+    <ModuleFooter
+      :cover="next.cover"
+      :name="next.name"
+      :slug="next.slug"
+    />
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import ModuleFooter from '@/modules/Footer'
+import { Banner as ComponentBanner } from '@/components/Banner'
+import { Gallery as ComponentGallery } from '@/components/Gallery'
+import { List as ComponentList } from '@/components/List'
+import { Tag as ComponentTag } from '@/components/Tag'
+import { Paragraph as ComponentParagraph } from '@/components/Paragraph'
+import { Screen as ComponentScreen } from '@/components/Screen'
+import { Title as ComponentTitle } from '@/components/Title'
 
 export default {
   name: 'Project',
+  components: {
+    ModuleFooter,
+    ComponentBanner,
+    ComponentGallery,
+    ComponentList,
+    ComponentTag,
+    ComponentParagraph,
+    ComponentScreen,
+    ComponentTitle,
+  },
   props: {
     slug: {
       type: String,
@@ -62,6 +82,21 @@ export default {
     project() {
       return this.projects.find(project => project.slug === this.slug)
     },
+    lists() {
+      return [
+        {
+          title: 'Role',
+          items: this.project.intro.roles,
+        },
+        {
+          title: 'What',
+          items: [this.project.type, this.project.year.toString()],
+        },
+      ]
+    },
+    next() {
+      return this.projects[(this.projects.indexOf(this.project) + 1) % this.projects.length]
+    },
   },
   created() {
     this.isNotFound = !this.projects.map(project => project.slug).includes(this.slug)
@@ -73,6 +108,10 @@ export default {
 .intro {
   padding: 20rem 22rem;
 
+  @include bp(lg) {
+    padding: 20rem 12rem;
+  }
+
   @include bp(md) {
     padding: 8rem 4rem;
   }
@@ -80,8 +119,16 @@ export default {
   @include bp(sm) {
     padding: 8rem 3.5rem 6rem;
   }
+}
 
-  .title::after {
+.title {
+  margin-bottom: 12rem;
+
+  @include bp(sm) {
+    margin-bottom: 4rem;
+  }
+
+  &::after {
     display: inline-block;
     width: 60px;
     height: 2px;
@@ -95,63 +142,39 @@ export default {
   }
 }
 
-.tag {
-  margin-bottom: 1rem;;
-  font-weight: 600;
-  font-size: 1.8rem;
-  text-transform: uppercase;
-
-  @include bp(sm) {
-    font-size: 1.6rem;
-  }
-
-  ~ .text {
-    line-height: 1.5;
-  }
-}
-
-.title {
-  margin-bottom: 12rem;
-  font-weight: 700;
-  font-size: 4rem;
-  font-family: $font-title;
-
-  @include bp(sm) {
-    margin-bottom: 4rem;
-    font-size: 2.2rem;
-  }
-}
-
-.sheet {
+.wrapper {
   display: grid;
   grid-auto-flow: column;
   grid-gap: 10rem;
   justify-content: space-between;
 
   @include bp(sm) {
+    grid-auto-columns: 1fr;
     grid-auto-flow: row;
     grid-gap: 4rem;
-    grid-template-rows: auto auto;
-    grid-template-columns: auto auto;
-  }
-}
 
-.section {
-
-  &:last-child {
-    @include bp(sm) {
-      grid-area: 2 / span 2;
+    > *:last-child {
+      grid-column: span 2;
     }
   }
 }
 
-.text {
-  font-weight: 400;
-  font-size: 1.8rem;
-  line-height: 1.25;
+.section {
+  padding: 20rem 0;
+
+  @include bp(md) {
+    padding: 8rem 0;
+  }
 
   @include bp(sm) {
-    font-size: 1.6rem;
+    padding: 6rem 0;
+  }
+
+  &:not(:last-of-type):nth-of-type(2n) {
+    color: $white;
+    background-color: $dark;
+
+    @include selection(dark);
   }
 }
 </style>
