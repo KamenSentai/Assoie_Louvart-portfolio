00574b365141f3fc4ef9fb29af58a038e38605ed
KamenSentai <cvt.alain@gmail.com>
2020-02-03 01:18:37 +0100

    feat(home): build year aside

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,18 @@
 <template>
   <ComponentHero :class="$style.container">
     <!-- TODO -->
-    <div />
+    <aside :class="$style.aside">
+      <div
+        v-for="(landing, index) in landings"
+        :key="landing.slug"
+        :class="$style.bloc"
+        :style="translation(index)"
+      >
+        <span :class="$style.tag">
+          {{ landing.year }}
+        </span>
+      </div>
+    </aside>
     <ul>
       <li
         v-for="landing in landings"
@@ -15,6 +26,7 @@
         </router-link>
       </li>
     </ul>
+    <aside :class="$style.navigation" />
     <ComponentIndicator :class="$style.indicator" />
   </ComponentHero>
 </template>
@@ -30,7 +42,71 @@ export default {
     ComponentHero,
     ComponentIndicator,
   },
-  computed: mapGetters('site', ['landings']),
+  data() {
+    return {
+      currentIndex: 0,
+      duration: 1000,
+      delay: 500,
+      isWheeling: false,
+    }
+  },
+  computed: {
+    ...mapGetters('site', ['landings']),
+    translation() {
+      const { currentIndex, duration } = this
+
+      return index => ({
+        transform: `translateY(${(index - currentIndex) * 100}vh)`,
+        opacity: currentIndex === index ? 1 : 0,
+        transition: `
+          transform ${duration}ms,
+          opacity ${duration / 2}ms ${currentIndex === index ? duration / 2 : 0}ms
+        `,
+        transitionTimingFunction: 'ease-in-out',
+        willChange: 'transform, opacity',
+      })
+    },
+  },
+  mounted() {
+    window.addEventListener('wheel', this.wheel)
+    window.addEventListener('keydown', this.press)
+  },
+  destroyed() {
+    window.removeEventListener('wheel', this.wheel)
+    window.removeEventListener('keydown', this.press)
+  },
+  methods: {
+    press({ keyCode }) {
+      switch (keyCode) {
+        case 38:
+          this.wheel({ deltaY: -1 })
+          break
+        case 40:
+          this.wheel({ deltaY: 1 })
+          break
+        // no default
+      }
+    },
+    wheel({ deltaY }) {
+      if (!this.isWheeling) {
+        if (deltaY !== 0) {
+          const { currentIndex, landings: { length } } = this
+
+          if (deltaY > 0) {
+            this.currentIndex = Math.min(length - 1, currentIndex + 1)
+          } else if (deltaY < 0) {
+            this.currentIndex = Math.max(0, currentIndex - 1)
+          }
+
+          this.isWheeling = true
+
+          setTimeout(() => {
+            this.isWheeling = false
+          }, this.duration + this.delay)
+        }
+      }
+    },
+  },
 }
 </script>
 
@@ -39,6 +115,35 @@ export default {
   grid-gap: 0 10rem;
   grid-template-columns: 12rem 1fr 12rem;
   overflow: hidden;
+}
+
+.aside {
+  position: relative;
+}
+
+.bloc {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.tag {
+  font-weight: 700;
+  font-size: 1.8rem;
+  font-family: $font-title;
+  letter-spacing: .5rem;
+  transform: rotate(-90deg);
+  transform-origin: center;
+}
+
+.navigation {
+  grid-row: span 2;
+  background-color: $white;
 }
 
 .indicator {
