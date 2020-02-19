69d7be4073af037047a9b74e619b7830115363f5
KamenSentai <cvt.alain@gmail.com>
2020-02-19 20:11:05 +0100

    refactor(screen): keep lateral visible

--- src/components/Screen/Screen.vue
+++ src/components/Screen/Screen.vue
@@ -2,11 +2,13 @@
   <div
     ref="container"
     :class="$style.container"
+    :style="{ paddingTop: padding, paddingBottom: padding }"
   >
     <img
       :class="$style.image"
       :src="src"
       :style="transform"
+      @load="load"
     >
   </div>
 </template>
@@ -30,20 +32,30 @@ export default {
   },
   data() {
     return {
-      rate: 25,
+      frameRatio: 0.9375,
+      imageRatio: 1,
     }
   },
   computed: {
+    padding() {
+      return `${100 / this.imageRatio / 2 * this.frameRatio}%`
+    },
     transform() {
-      const { rate, scrollY, windowHeight } = this
+      const { frameRatio, scrollY, windowHeight } = this
       const { clientHeight, offsetTop } = this.$refs.container || { clientHeight: 0, offsetTop: 0 }
-      const from = offsetTop - windowHeight
-      const to = offsetTop + clientHeight
-      const delta = rate * ((scrollY - from) / (to - from) - 0.5)
+      const from = offsetTop - (windowHeight - clientHeight)
+      const to = offsetTop
+      const delta = ((scrollY - from) / (to - from) - 0.5) * (1 - frameRatio) * 100
 
       return {
-        transform: `translateY(${delta}%) scale(1.${rate.toString().split('.').join('')})`,
+        transform: `translateY(${delta}%)`,
       }
+    },
+  },
+  methods: {
+    load(event) {
+      const { srcElement: { naturalWidth, naturalHeight } } = event
+      this.imageRatio = naturalWidth / naturalHeight
     },
   },
 }
@@ -51,13 +63,15 @@ export default {
 
 <style lang="scss" module>
 .container {
+  position: relative;
   overflow: hidden;
   font-size: 0;
+  @include centralizer;
 }
 
 .image {
+  position: absolute;
   width: 100%;
-  height: 100%;
   transform-origin: center;
   will-change: transform;
 }
