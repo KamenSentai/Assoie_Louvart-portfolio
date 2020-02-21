c1405e1ff745acae58391ab87c6f33c797521828
KamenSentai <cvt.alain@gmail.com>
2020-02-21 16:44:10 +0100

    feat(404): build page

--- src/store/site/state.js
+++ src/store/site/state.js
@@ -1,9 +1,11 @@
 import about from './states/about'
+import error from './states/error'
 import home from './states/home'
 import projects from './states/projects'
 
 export default {
   about,
+  error,
   home,
   projects,
   savedIndex: 0,
