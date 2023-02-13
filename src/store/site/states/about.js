941a50e7ae84122bb0526990b50f7b071b127c9a
KamenSentai <cvt.alain@gmail.com>
2023-02-13 11:03:52 +0100

    feat(about): update content

--- src/store/site/states/about.js
+++ src/store/site/states/about.js
@@ -1,7 +1,7 @@
 import env from '@/utils/env'
 import separator from '@/utils/separator'
 
-const description = 'I\'m Assoïe, a Paris based full-time Freelance Product Designer. I\'ve studied during the last 5 years at HETIC. That experience had let me discover the complexity of digital design. Now, the work I produce combines visual design and product design. I am passionate about design and looking forward to work with talented people on interesting projects.'
+const description = 'I\'m Assoïe, a Paris based full-time Freelance Product Designer and UX/UI Designer. Curious and invested designer, my goal is to design and improve the experiences you offer to your users. I intervene on the research phases, but also on the UX/UI design phases. Passionate about design, I like to work on the whole project. My numerous experiences in agencies and at the client\'s allow me to adapt to any situation and type of project.'
 
 export default {
   content: [
@@ -20,6 +20,24 @@ export default {
         class: 'grid',
         items: [
           [
+            'FREELANCE (+5 years)',
+            '2018 - 2023',
+            'Product Designer / UX/UI Designer',
+            '🌍',
+          ],
+          [
+            'USE ALL FIVE',
+            '2022 - 2023',
+            'UX/UI Designer',
+            'Los Angeles 🌇',
+          ],
+          [
+            'VO',
+            '2022',
+            'UX/UI Designer',
+            'Paris 🥐',
+          ],
+          [
             'ULTRANOIR',
             '2021',
             'Junior Visual Designer',
@@ -30,6 +48,12 @@ export default {
             '2020',
             'Visual Designer Intern',
             'New York 🗽',
+          ],
+          [
+            'YUKEY',
+            '2019',
+            'Product Designer',
+            'Paris 🥐',
           ],
           [
             'ET SI',
@@ -47,20 +71,6 @@ export default {
       },
     },
     {
-      title: {
-        tag: 'h2',
-        text: 'In details',
-      },
-      paragraph: 'Two years ago, I chose to do my internship at UNILOG company in Bangalore, India, as Junior Designer. Working with passionated people helped me improve my skills. It was my first time in India and it became my life strongest experience. My second internship was in Paris at ET SI, also as Junior Designer for 5 months. For my last internship I had the chance to intern at FANTASY as a Visual Designer for 6 months. This experience allowed me to work on great project for famous brands with amazing designers.',
-    },
-    {
-      title: {
-        tag: 'h2',
-        text: 'And today ?',
-      },
-      paragraph: 'I just finished my 1 year apprenticeship at ULTRANOIR where I had the chance to work for luxury brands. Actually, I am available for freelance. Please feel free to contact me for more informations. I\'ll be more than happy to chat with you about any opportunities!',
-    },
-    {
       isSocial: true,
       title: {
         tag: 'h2',
@@ -70,7 +80,7 @@ export default {
         class: 'link',
         items: [
           {
-            name: 'Email',
+            name: 'Mail',
             title: 'louvartassoie@gmail.com',
             link: 'mailto:louvartassoie@gmail.com',
           },
